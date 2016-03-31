4817c6816f6b9b28af2da3a3fabc9d76f10bb898
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-03-31 16:37:01 +0000

    Making json output

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,7 +9,7 @@ module.exports = function (app, passport) {
 			var agent = req.headers["user-agent"];
 			var ip = req.ip;
 			var out = {
-			    "ipaddress": (req.ip.startsWith(":fff::") ? ip.substring(6, ip.length) : ip) ,
+			    "ipaddress": (req.ip.startsWith(":ffff::") ? ip.substring(7, ip.length) : ip) ,
 			    "language": req.headers["accept-language"].split(",")[0],
 			    "software": agent.slice(agent.indexOf("(")+1, agent.indexOf(")"))
 			}
