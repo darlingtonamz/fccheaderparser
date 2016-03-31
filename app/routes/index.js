2d6d4cfa1ba1a4a1b809cfb2d5c0af132464b1bb
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-03-31 16:25:00 +0000

    Making json output

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,8 +6,13 @@ var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 module.exports = function (app, passport) {
 	app.route('/')
 		.get(function (req, res) {
-			var ip = req.connection.remoteAddress;
-			res.send("IP: "+ip)
+			var agent = req.headers["user-agent"];
+			var out = {
+			    "ipaddress": req.ip,
+			    "language": req.headers["accept-language"].split(",")[0],
+			    "software": agent.slice(agent.indexOf("(")+1, agent.indexOf(")"))
+			}
+			res.send(out)
 			//res.sendFile(path + '/public/index.html');
 		});
 };
