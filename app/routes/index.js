29231f3e0673f748f7b089c2b317fdaee085ae75
Amanze Ogbonna <darlingtonamz@gmail.com>
2016-04-01 12:52:54 +0000

    Parsing issue corrected for ipaddress

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,8 +9,7 @@ module.exports = function (app, passport) {
 			var agent = req.headers["user-agent"];
 			var ip = req.ip;
 			var out = {
-					"test": (req.ip.startsWith("::ffff:")
-			    "ipaddress": (req.ip.startsWith("::ffff:") ? ip.substring(7, ip.length) : ip) ,
+			    "ipaddress": (ip.startsWith("::ffff:") ? ip.substring(7, ip.length) : ip) ,
 			    "language": req.headers["accept-language"].split(",")[0],
 			    "software": agent.slice(agent.indexOf("(")+1, agent.indexOf(")"))
 			}
